a2d980344bfa64a872c05d7dbce70712fe209ef6
AleksLap <a.lapanovich@gmail.com>
2021-10-17 19:50:15 +0300

    feat: add slider scroll (Sun, Oct 17, 2021 19:50 PM)

--- cssMemSlider/script.js
+++ cssMemSlider/script.js
@@ -2,16 +2,45 @@ const slider = document.getElementById('slider');
 const sliderInner = document.querySelector('.slider-inner');
 const images = document.querySelectorAll('.img');
 const controlItems = document.querySelectorAll('.control-item');
+const spans = document.querySelectorAll('.span');
+let index = 0;
+let width;
 
 function init() {
-  const width = slider.offsetWidth;
+  width = slider.offsetWidth;
   sliderInner.style.width = width * images.length + 'px';
   images.forEach(item => {
     item.style.width = width + 'px';
     item.style.height = 'auto';
   })
-  console.log(width)
+};
+
+function removeActiveClass() {
+  spans.forEach(item => {
+    item.classList.remove('span-active');
+  })
 }
+
+function rollSlider() {
+  sliderInner.style.transform = `translate(-${index * width}px)`;
+  console.log('toleft: ' + `translate(-${index * width}px)`)
+}
+
+controlItems.forEach(item => {
+  item.addEventListener('click', function() {
+    let controlInd = +this.dataset.index;
+    console.log(controlInd)
+    if(index === controlInd) {
+      return;
+    } else {
+      index = controlInd;
+      removeActiveClass();
+      this.querySelector('.span').classList.add('span-active');
+      console.log(index)
+      rollSlider();
+    }
+  })
+});
 
 init();
 
